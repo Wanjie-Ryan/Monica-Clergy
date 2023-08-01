a70206a5aa80dbc63bd4dfe2dcffaefab4859a9c
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-01 14:59:08 +0300

    clergy-admin

---
+++ src/components/Events/General-Events/GE.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import './Ge.css'
+import generalEvents from '../../../Assets/Events/general_events.jpg'
+import {Link} from 'react-router-dom'
+ 
+
+
+function GeneralEvents (){
+
+    return(
+
+        <>
+
+
+            <section className="general-events">
+
+                <p className='gen-title'>General Events</p>
+                <p className='event-desc'>These are events that involve the whole of the church</p>
+
+                <div className='all-events'>
+
+                    <div className="all-events-container">
+
+                        <p className='upcoming-title'>Current Events</p>
+
+
+                        <div className='upcoming-events'>
+
+
+                            <div className="upcoming-events-container">
+
+                                <div className='img-cont'>
+                                    
+                                    <img src ={generalEvents} alt =''  className='img-events'/>
+                                    <Link to = '/general-events/:id' className='explore'>Explore</Link>
+
+                                </div>
+
+
+                                <p className='event-title'>Event Title:</p>
+                                <p className='event-title'>Event Description:</p>
+
+                            </div>
+
+                            
+
+                            
+                        
+                        </div>
+
+
+                        <p className='upcoming-title'>Upcoming Events</p>
+
+                        <div className='upcoming-events'>
+
+                            <div className="upcoming-events-container">
+
+                                <div className='img-cont'>
+                                        
+                                    <img src ={generalEvents} alt =''  className='img-events'/>
+                                    <Link to = '/general-events/:id' className='explore'>Explore</Link>
+
+                                </div>
+
+
+                                <p className='event-title'>Event Title:</p>
+                                <p className='event-title'>Event Description:</p>
+                                <p>Actual Date for Event:</p>
+                                <p>Registration Deadline:</p>
+
+                            </div>
+
+                            
+
+                                   
+                        </div>
+
+
+                        <p className='upcoming-title'>Past Events</p>
+
+                        <div className='upcoming-events'>
+
+                            <div className="upcoming-events-container">
+
+                                <div className='img-cont'>
+                                        
+                                    <img src ={generalEvents} alt =''  className='img-events'/>
+                                    <Link to = '/general-events/:id' className='explore'>Explore</Link>
+
+                                </div>
+
+
+                                <p className='event-title'>Event Title:</p>
+                                <p className='event-title'>Event Description:</p>
+                                
+
+                            </div>
+
+
+                                
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
+        
+        
+        
+        </>
+    )
+}
+
+
+export default GeneralEvents
