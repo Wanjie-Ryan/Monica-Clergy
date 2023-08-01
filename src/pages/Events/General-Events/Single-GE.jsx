83bc11c8d9d473c0a571ef6615ba8d51a6e589ba
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-01 15:07:49 +0300

    clergy-admin

---
+++ src/pages/Events/General-Events/Single-GE.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from '../../../../../../Monica Client/user/src/Components/navbar/nav'
+import SingleGeneralEvents from '../../../../../../Monica Client/user/src/Components/Events/General-Events/Single-GE'
+import Footer from '../../../../../../Monica Client/user/src/Components/footer/footer'
+
+function GEPages() {
+
+
+  return (
+
+
+    <>
+
+        <Navbar/>
+        < SingleGeneralEvents/>
+        <Footer/>
+
+    
+    
+    </>
+
+
+
+  )
+}
+
+export default GEPages 
