d123c215e1a38b1329c829e3e6fe15a4c9920eef
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-01 13:15:23 +0300

    clergy-admin

--- src/components/Events/events.jsx
+++ src/components/Events/events.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import churchview from '../../Assets/homeImages/church_view.jpg'
+import GE from '../../Assets/Events/general_events.jpg'
+
+
 
 
 
@@ -12,6 +15,136 @@ function Events (){
 
         <>
 
+            <section className='projects'>
+
+                <div className="projects-container">
+
+                    <div className="actual-projects">
+
+                        <div className="project-img">
+
+                            <img src ={GE} alt='church' className='img-proj'/>
+
+                            <div className="img-text">
+
+                                <p className='project-desc'>General Events</p>
+
+                            </div>
+
+                            <div className="up-del">
+                                
+                                <p>See More</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="project-img">
+
+                            <img src ={churchview} alt='church' className='img-proj'/>
+
+                            <div className="img-text">
+
+                                <p className='project-desc'>Events for Men</p>
+
+                            </div>
+
+                            <div className="up-del">
+                                
+                                <p>See More</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="project-img">
+
+                            <img src ={churchview} alt='church' className='img-proj'/>
+
+                            <div className="img-text">
+
+                                <p className='project-desc'>Events for Ladies</p>
+
+                            </div>
+
+                            <div className="up-del">
+                                
+                                <p>See More</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="project-img">
+
+                            <img src ={churchview} alt='church' className='img-proj'/>
+
+                            <div className="img-text">
+
+                                <p className='project-desc'>Events for Youths</p>
+
+                            </div>
+
+                            <div className="up-del">
+                                
+                                <p>See More</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="project-img">
+
+                            <img src ={churchview} alt='church' className='img-proj'/>
+
+                            <div className="img-text">
+
+                                <p className='project-desc'>Events for Teens</p>
+
+                            </div>
+
+                            <div className="up-del">
+                                
+                                <p>See More</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="project-img">
+
+                            <img src ={churchview} alt='church' className='img-proj'/>
+
+                            <div className="img-text">
+
+                                <p className='project-desc'>Events for Kids</p>
+
+                            </div>
+
+                            <div className="up-del">
+                                
+                                <p>See More</p>
+
+                            </div>
+
+                        </div>
+
+
+
+
+
+
+
+                    </div>
+
+
+                </div>
+
+
+
+
+
+            </section>
            
 
         </>
