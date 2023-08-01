cca917950f5741aa93727dd9f2389e7d7e7391ec
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-01 19:50:19 +0300

    clergy-admin

--- src/components/feedback/fb.jsx
+++ src/components/feedback/fb.jsx
@@ -6,9 +6,55 @@ import './fb.css'
 
 function FeedBack (){
 
+    const tableData = [
+        { id: 1, name: 'John Doe', email: 'john@example.com', message: 'Hello, world!' },
+        { id: 2, name: 'Jane Smith', email: 'jane@example.com', message: 'Nice project!' },
+        // Add more data as needed
+      ];
+
     return(
 
         <>
+
+            <section className='projects'>
+
+                <p className='feedback-title'>Feedback from the congregants</p>
+
+                <div className="projects-container">
+
+                    
+                    <div className="table-container">
+
+                        <table className="custom-table">
+                            <thead>
+
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Message</th>
+                                </tr>
+
+                            </thead>
+
+                            <tbody>
+
+                                {tableData.map((dataItem) => (
+                                    <tr key={dataItem.id}>
+                                    <td>{dataItem.id}</td>
+                                    <td>{dataItem.name}</td>
+                                    <td>{dataItem.email}</td>
+                                    <td>{dataItem.message}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+
+                        </table>
+
+                    </div>
+
+                </div>
+            </section>
 
         </>
     )
