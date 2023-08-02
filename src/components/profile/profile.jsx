a9dfc9f747ff2e48359ab25c0e455773489e764a
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-02 08:44:29 +0300

    clergy-admin

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './profile.css'
 import Avatar from '../../Assets/DefaultAvatar.png'
-
+import {AiOutlineEye} from 'react-icons/ai'
 
 
 
@@ -24,7 +24,42 @@ function Profile (){
                         
                     </div>
 
+                        <div className="update">
 
+
+                            <div className='pwd'>
+
+                                <label className='update-text'>Change Name</label>
+
+                                <input type='text' placeholder='Enter Your Name' className='pwd-input' />
+
+
+                            </div>
+
+                            <div className="pwd">
+
+                                <label className='update-text'>Change Photo</label> 
+                                <input
+
+                                    type="file"
+                                    id="imageFile"
+                                    accept="image/*"
+                                    className='pwd-input'
+                                />
+                                    
+
+                            </div>
+
+                            <div className='pwd update-pwd'>
+
+                                <label className='update-text'>Change Password</label>
+                                <input type='password' placeholder='Password' className='pwd-input' />
+
+                                <AiOutlineEye className='pwd-icon'/>
+                            </div>
+
+
+                        </div>
 
                 </div>
 
