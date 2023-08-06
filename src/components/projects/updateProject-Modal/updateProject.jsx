2d4d97f63354f57b0ebc26fa51bdcfa5635847f9
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-06 17:16:57 +0300

    clergy-integration

--- src/components/projects/updateProject-Modal/updateProject.jsx
+++ src/components/projects/updateProject-Modal/updateProject.jsx
@@ -1,34 +1,26 @@
-import React, { useState } from 'react';
-import '../projects.css'
-import axios from 'axios'
-import {  AiOutlineLoading3Quarters } from "react-icons/ai";
+import React, { useState } from "react";
+import "../projects.css";
+import axios from "axios";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-
 function UpdateProjectModal({ isOpen, onClose, projectId, token }) {
-
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const [image, setImage] = useState();
   const [loading, setLoading] = useState(false);
-  const [errMsg, seterrMsg] = useState('')
+  const [errMsg, seterrMsg] = useState("");
 
   const handleTitleChange = (e) => {
-
     setTitle(e.target.value);
-
   };
 
-  
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    e.preventDefault()
-
-    if(!title || !image){
-
-      toast.error('Please fill in all the fields')
-      return 
+    if (!title || !image) {
+      toast.error("Please fill in all the fields");
+      return;
     }
 
     const config = {
@@ -37,31 +29,54 @@ function UpdateProjectModal({ isOpen, onClose, projectId, token }) {
       },
     };
 
+    setLoading(true);
 
+    try {
+      const formData = new FormData();
+      formData.append("file", image);
 
+      formData.append("upload_preset", "pq4z6rjr");
 
+      const imageData = await axios.post(
+        "https://api.cloudinary.com/v1_1/djgk2k4sw/image/upload",
+        formData
+      );
 
-    
+      const projectUpdateData = {
+        title: title,
+        image: imageData.data.secure_url,
+      };
+
+      const UpdateData = await axios.patch(
+        `http://localhost:3005/api/clergy/projects/updateproject/${projectId}`,
+        projectUpdateData,
+        config
+      );
+
+      // console.log(UpdateData)
+
+      toast.success("Project has been updated successfully");
+
+      setLoading(false);
+    } catch (err) {
+      // console.log(err)
+      seterrMsg(err.response.data.error);
+      setLoading(false);
+    }
+
     onClose();
-
   };
 
   if (!isOpen) {
-
     return null;
-
   }
 
   return (
-
     <div className="modal">
-
       <form className="modal-content">
-
         <h2>Update Project</h2>
 
         <div className="modal-input">
-
           <label htmlFor="title">Title:</label>
 
           <input
@@ -71,32 +86,32 @@ function UpdateProjectModal({ isOpen, onClose, projectId, token }) {
             onChange={handleTitleChange}
             placeholder="Enter project title"
           />
-
         </div>
 
-        
         <div className="modal-input">
-
           <label htmlFor="imageFile">Image:</label>
 
-            <input
-              type="file"
-              id="imageFile"
-              accept="image/*"
-              onChange={(e) => {
-                setImage(e.target.files[0]);
-              }}
-            
-            />
-
+          <input
+            type="file"
+            id="imageFile"
+            accept="image/*"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+          />
         </div>
 
-        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={handleSubmit}>
+          {loading ? (
+            <AiOutlineLoading3Quarters className="loading-icon" />
+          ) : (
+            "Submit"
+          )}
+        </button>
         <button onClick={onClose}>Cancel</button>
-
+        {errMsg ? <p className="error-msg">{errMsg}</p> : ""}
       </form>
     </div>
-
   );
 }
 
