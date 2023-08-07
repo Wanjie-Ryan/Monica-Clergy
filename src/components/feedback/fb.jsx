e229077faae808e7532e8247923b7f864bb500df
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-07 16:02:04 +0300

    clergy-integration

--- src/components/feedback/fb.jsx
+++ src/components/feedback/fb.jsx
@@ -1,181 +1,128 @@
-import React, {useState, useEffect} from 'react'
-import './fb.css'
-import {AiFillPrinter} from 'react-icons/ai'
-import axios from 'axios'
-import Cookies from 'js-cookie'
-import {  AiOutlineLoading3Quarters } from "react-icons/ai";
-import { Document, Page, Text, View, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+import React, { useState, useEffect } from "react";
+import "./fb.css";
+import { AiFillPrinter } from "react-icons/ai";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFDownloadLink,
+} from "@react-pdf/renderer";
 
+function FeedBack() {
+  const [fb, setFb] = useState([]);
+  const [errMsg, setErrMsg] = useState();
+  const [Loading, setLoading] = useState(false);
+  const [pdfData, setPdfData] = useState([]);
+  const [pdfLoading, setPdfLoading] = useState(false);
 
-function FeedBack (){
+  useEffect(() => {
+    const getFeedbacks = async () => {
+      setLoading(true);
 
-    const [fb, setFb] = useState([])
-    const [errMsg, setErrMsg] = useState()
-    const [Loading, setLoading] = useState(false);
+      try {
+        const token = Cookies.get().clergyToken;
 
-    useEffect(()=>{
+        const Allfeedbacks = await axios.get(
+          "http://localhost:3005/api/user/allfeedbacks",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
-        const getFeedbacks = async()=>{
+        // console.log(Allfeedbacks.data.getFeedbacks)
 
-            setLoading(true)
+        setFb(Allfeedbacks.data.getFeedbacks);
+        setPdfData(Allfeedbacks.data.getFeedbacks);
+        setPdfLoading(false);
+        setLoading(false);
+      } catch (err) {
+        // console.log(err)
+        setLoading(false);
+        setErrMsg("Sorry could not be able to get feedbacks");
+      }
+    };
 
-            try{
+    getFeedbacks();
+  }, []);
 
-                const token =Cookies.get().clergyToken
+  const downloadFeedbacks = () => {
+    setLoading(true);
 
-                const Allfeedbacks = await axios.get('http://localhost:3005/api/user/allfeedbacks', {headers:{
-                    Authorization:`Bearer ${token}`
-                }})
+    // Convert the feedback data to plain text
+    const plainTextData = fb.map(
+      (feedback, index) =>
+        `${index + 1}. Name: ${feedback.name}, Email: ${
+          feedback.email
+        }, Message: ${feedback.message}\n`
+    );
 
-                // console.log(Allfeedbacks.data.getFeedbacks)
+    const plainText = plainTextData.join("");
 
-                setFb(Allfeedbacks.data.getFeedbacks)
+    const blob = new Blob([plainText], { type: "text/plain" });
 
-                setLoading(false)
+    const url = URL.createObjectURL(blob);
 
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "feedbacks.txt";
+    document.body.appendChild(a);
+    a.click();
 
-            }
+    URL.revokeObjectURL(url);
+    a.remove();
 
-            catch(err){
+    setLoading(false);
+  };
 
-                // console.log(err)
-                setLoading(false)
-                setErrMsg('Sorry could not be able to get feedbacks')
-            }
-        }
+  return (
+    <>
+      <section className="projects">
+        <p className="feedback-title">Feedback from the congregants</p>
 
-        getFeedbacks()
+        <div className="projects-container">
+          <div className="table-container">
+            <AiFillPrinter className="printer" onClick={downloadFeedbacks} />
 
+            <table className="custom-table">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Message</th>
+                </tr>
+              </thead>
 
-    },[])
-
-    //PDF
-
-    const styles = StyleSheet.create({
-
-        page: {
-        padding: 20,
-        },
-
-        heading: {
-        fontSize: 18,
-        marginBottom: 10,
-        },
-
-        table: {
-        display: 'table',
-        width: '100%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderRightWidth: 0,
-        borderBottomWidth: 0,
-        },
-
-        tableRow: {
-        margin: 'auto',
-        flexDirection: 'row',
-        },
-
-        tableCell: {
-        margin: 'auto',
-        fontSize: 12,
-        padding: 5,
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0,
-        },
-        
-  });
-
-  const PDFDocument = (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View>
-          <Text style={styles.heading}>Feedback from the Congregants</Text>
-        </View>
-        <View style={styles.table}>
-          {fb.map((feedback, index) => (
-            <View style={styles.tableRow} key={feedback.id}>
-              <View style={styles.tableCell}>{index + 1}</View>
-              <View style={styles.tableCell}>{feedback.name}</View>
-              <View style={styles.tableCell}>{feedback.email}</View>
-              <View style={styles.tableCell}>{feedback.message}</View>
-            </View>
-          ))}
-        </View>
-      </Page>
-    </Document>
+              {Loading ? (
+                <AiOutlineLoading3Quarters className="loading-icon" />
+              ) : fb.length === 0 ? (
+                <tr>
+                  <td colSpan="4">No feedbacks available</td>
+                </tr>
+              ) : (
+                fb.map((feedback, index) => (
+                  <tr key={feedback.id}>
+                    <td>{index + 1}</td>
+                    <td>{feedback.name}</td>
+                    <td>{feedback.email}</td>
+                    <td>{feedback.message}</td>
+                  </tr>
+                ))
+              )}
+              {errMsg && <p className="error-msg">{errMsg}</p>}
+            </table>
+          </div>
+        </div>
+      </section>
+    </>
   );
-
-
-
-    
-    return(
-
-        <>
-
-            <section className='projects'>
-
-                <p className='feedback-title'>Feedback from the congregants</p>
-
-                <div className="projects-container">
-
-                    <div className="table-container">
-
-                        <AiFillPrinter className='printer'/>
-
-                        <table className="custom-table">
-                            <thead>
-
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Message</th>
-                                </tr>
-
-                            </thead>
-
-
-                            {Loading ? (
-
-                                <AiOutlineLoading3Quarters className="loading-icon" />
-
-                                ) : fb.length === 0 ? (
-
-                                <tr>
-
-                                    <td colSpan="4">No feedbacks available</td>
-
-                                </tr>
-
-                                ) : (
-
-                                fb.map((feedback, index) => (
-
-                                    <tr key={feedback.id}>
-
-                                    <td>{index + 1}</td>
-                                    <td>{feedback.name}</td>
-                                    <td>{feedback.email}</td>
-                                    <td>{feedback.message}</td>
-
-                                    </tr>
-                                ))
-
-                            )}
-                            {errMsg && <p className='error-msg'>{errMsg}</p>}
-
-                        </table>
-
-                    </div>
-
-                </div>
-            </section>
-
-        </>
-    )
 }
 
-export default FeedBack
+export default FeedBack;
