46ff61c262cb69bcf77f520671828654cbe27f03
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-09 13:35:32 +0300

    clergy-integration

--- src/components/Events/events.jsx
+++ src/components/Events/events.jsx
@@ -1,23 +1,19 @@
-import React, {useState, useEffect} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import churchview from '../../Assets/homeImages/church_view.jpg'
-import GE from '../../Assets/Events/general_events.jpg'
-import men from '../../Assets/Events/men.jpg'
-import WE from '../../Assets/Events/women_events.jpg'
-import './events.css'
-import Youths from '../../Assets/homeImages/youth.jpg'
-import kids from '../../Assets/homeImages/sunday school.jpg'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import churchview from "../../Assets/homeImages/church_view.jpg";
+import GE from "../../Assets/Events/general_events.jpg";
+import men from "../../Assets/Events/men.jpg";
+import WE from "../../Assets/Events/women_events.jpg";
+import "./events.css";
+import Youths from "../../Assets/homeImages/youth.jpg";
+import kids from "../../Assets/homeImages/sunday school.jpg";
+import axios from "axios";
 import Cookies from "js-cookie";
 
+function Events() {
+  const navigate = useNavigate();
 
-
-
-function Events (){
-
-    const navigate = useNavigate()
-
-    useEffect(() => {
+  useEffect(() => {
     const checkAuth = async () => {
       if (
         !Cookies.get().clergyToken ||
@@ -43,132 +39,86 @@ function Events (){
     checkAuth();
   }, [navigate]);
 
+  return (
+    <>
+      <section className="projects">
+        <div className="projects-container">
+          <div className="add-events">
+            <p className="add-p">Events</p>
+          </div>
 
+          <div className="actual-projects">
+            <div className="project-img">
+              <p className="project-desc-events">General Events</p>
 
+              <img src={GE} alt="church" className="img-proj" />
 
+              <div className="up-del">
+                <Link to="/general-events" className="links-events">
+                  <p className="see-more-events">See More</p>
+                </Link>
+              </div>
+            </div>
 
+            <div className="project-img">
+              <p className="project-desc-events">Events for Men</p>
+              <img src={men} alt="church" className="img-proj" />
 
-    return(
+              <div className="up-del">
+                <Link to="/men-events" className="links-events">
+                  <p className="see-more-events">See More</p>
+                </Link>
+              </div>
+            </div>
 
+            <div className="project-img">
+              <p className="project-desc-events">Events for Ladies</p>
+              <img src={WE} alt="church" className="img-proj" />
 
-        <>
+              <div className="up-del">
+                <Link to="/ladies-events" className="links-events">
+                  <p className="see-more-events">See More</p>
+                </Link>
+              </div>
+            </div>
 
-            <section className='projects'>
+            <div className="project-img">
+              <p className="project-desc-events">Events for Youths</p>
+              <img src={Youths} alt="church" className="img-proj" />
 
-                <div className="projects-container">
+              <div className="up-del">
+                <Link to="/youth-events" className="links-events">
+                  <p className="see-more-events">See More</p>
+                </Link>
+              </div>
+            </div>
 
-                    <div className='add-events'><p className='add-p'>Events</p></div>
+            <div className="project-img">
+              <p className="project-desc-events">Events for Teens</p>
+              <img src={churchview} alt="church" className="img-proj" />
 
-                    <div className="actual-projects">
+              <div className="up-del">
+                <Link to="/teen-events" className="links-events">
+                  <p className="see-more-events">See More</p>
+                </Link>
+              </div>
+            </div>
 
-                        <div className="project-img">
+            <div className="project-img">
+              <p className="project-desc-events">Events for Kids</p>
+              <img src={kids} alt="church" className="img-proj" />
 
-                            <p className='project-desc-events'>General Events</p>
-
-                            <img src ={GE} alt='church' className='img-proj'/>
-
-
-                            <div className="up-del">
-                                
-                                <Link to='/general-events' className='links-events'><p className='see-more-events'>See More</p></Link>
-
-                            </div>
-
-                        </div>
-
-                        <div className="project-img">
-
-                            <p className='project-desc-events'>Events for Men</p>
-                            <img src ={men} alt='church' className='img-proj'/>
-
-
-                            <div className="up-del">
-                                
-                                <Link to='/men-events' className='links-events'><p className='see-more-events'>See More</p></Link>
-
-                            </div>
-
-                        </div>
-
-                        <div className="project-img">
-
-                            <p className='project-desc-events'>Events for Ladies</p>
-                            <img src ={WE} alt='church' className='img-proj'/>
-
-                           
-
-                            <div className="up-del">
-                                
-                                <Link to='/ladies-events' className='links-events'><p className='see-more-events'>See More</p></Link>
-
-                            </div>
-
-                        </div>
-
-                        <div className="project-img">
-
-                            <p className='project-desc-events'>Events for Youths</p>
-                            <img src ={Youths} alt='church' className='img-proj'/>
-
-                            
-
-                            <div className="up-del">
-                                
-                                <Link to='/youth-events' className='links-events'><p className='see-more-events'>See More</p></Link>
-
-                            </div>
-
-                        </div>
-
-                        <div className="project-img">
-
-                            <p className='project-desc-events'>Events for Teens</p>
-                            <img src ={churchview} alt='church' className='img-proj'/>
-
-                            
-
-                            <div className="up-del">
-                                
-                                <Link to='/teen-events' className='links-events'><p className='see-more-events'>See More</p></Link>
-
-                            </div>
-
-                        </div>
-
-                        <div className="project-img">
-
-                            <p className='project-desc-events'>Events for Kids</p>
-                            <img src ={kids} alt='church' className='img-proj'/>
-
-                           
-                            <div className="up-del">
-                                
-                                <Link to='/kids-events' className='links-events'><p className='see-more-events'>See More</p></Link>
-
-                            </div>
-
-                        </div>
-
-
-
-
-
-
-
-                    </div>
-
-
-                </div>
-
-
-
-
-
-            </section>
-           
-
-        </>
-    )
+              <div className="up-del">
+                <Link to="/kids-events" className="links-events">
+                  <p className="see-more-events">See More</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Events
+export default Events;
